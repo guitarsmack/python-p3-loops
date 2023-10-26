74f98409f555fb31626a6afe4770bdedd9022186
Michael Alan Stowe <mastowe94@gmail.com>
2023-10-26 02:26:54 -0500

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    number = 10
+    while number > 1:
+        print(number)
+        number-=1
+    print(number)
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_list = [num * num for num in int_list]
+    return new_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for position in range(100):
+        num = position + 1
+        three = num % 3 == 0
+        five = num % 5 == 0
+        # ipdb.set_trace()
+        if three and five:
+            print("FizzBuzz")
+        elif three and not five:
+            print("Fizz")
+        elif five and not three:
+            print("Buzz")
+        else:
+            print(num)
+        
+
